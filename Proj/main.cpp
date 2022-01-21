4950610d98f979f8d3a92bb273d73d000bd68d1d
Mohammad Dehghani <MohammadTtay@gmail.com>
2022-01-21 15:46:41 +0330

    Read Data From JSON File

--- Proj/main.cpp
+++ Proj/main.cpp
@@ -1,8 +1,45 @@
-#include <QCoreApplication>
+#include <bits/stdc++.h>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+#include "nlohmann/json.hpp"
 
-    return a.exec();
+#include"Users.h"
+
+#include<fstream>
+
+using namespace std;
+using json = nlohmann::json;
+
+int readFiles(vector < Users * > & Database);
+int main() {
+    vector < Users * > Database;
+
+    if (!readFiles(Database)) { //Read The Information From "users.jason" When Program Starts
+        return 0;
+    }
+
+    for (auto x: Database) x -> printData();
+
+    return 0;
+}
+
+int readFiles(vector < Users * > & Database) {
+    ifstream file("../users.json");
+    if (file.is_open()) {
+        char temp;
+        string tempLine;
+        while (true) {
+            file >> temp;
+            if (temp == ']') break;
+            getline(file, tempLine, '}');
+            tempLine.push_back('}');
+
+            Users * tempUser = new Users(json::parse(tempLine));
+            Database.push_back(tempUser);
+
+        }
+
+        file.close();
+        return 1;
+    } else return 0;
+
 }
